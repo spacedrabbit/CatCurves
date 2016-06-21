de2d18d1f74e2950ca15fde0a0001d6b4ed1b64d
Louis Tur <louis.tur@gmail.com>
2016-06-21 17:49:15 -0400

    makes ref points a little easier to write out

--- SRControlPoints/ViewController.swift
+++ SRControlPoints/ViewController.swift
@@ -9,13 +9,37 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+internal struct RectFramer {
+  var viewRef: UIView
   
+  let maxY, maxX, midX, midY, minX, minY: CGFloat
+  let origin, bottomRight, topLeft, topRight, bottomLeft: CGPoint
+  
+  internal init(withView view: UIView) {
+    viewRef = view
+    
+    maxY = CGRectGetMaxY(view.bounds)
+    maxX = CGRectGetMaxX(view.bounds)
+    minX = CGRectGetMinX(view.bounds)
+    minY = CGRectGetMinY(view.bounds)
+    midX = CGRectGetMidX(view.bounds)
+    midY = CGRectGetMidY(view.bounds)
+    origin = CGPointMake(minX, maxY)
+    bottomRight = CGPointMake(maxX, maxY)
+    topRight = CGPointMake(maxX, midY)
+    topLeft = CGPointMake(minX, midY)
+    bottomLeft = origin
+  }
+  
+}
+
+class ViewController: UIViewController {
+  var frameRef: RectFramer?
   
   // MARK: - Variables
   let Oatmeal: UIColor = UIColor(red: 255.0/255.0, green: 240.0/255.0, blue: 165.0/255.0, alpha: 1.0)
   let Knoll: UIColor = UIColor(red: 70.0/255.0, green: 137.0/255.0, blue: 102.0/255.0, alpha: 1.0)
-
+  var sizingToken: dispatch_once_t = 0
   
   // MARK: - View Lifecycle
   override func viewDidLoad() {
@@ -30,19 +54,28 @@ class ViewController: UIViewController {
   }
   
   override func viewDidLayoutSubviews() {
-    self.drawLineIn(self.drawingView)
+    dispatch_once(&sizingToken) { 
+      self.frameRef = RectFramer(withView: self.drawingView)
+    }
+    
+    if self.frameRef != nil {
+      self.drawLineIn(withRectRef: self.frameRef!)
+    }
   }
   
   
   // MARK: - Drawing
-  internal func drawLineIn(view: UIView) {
-//    view.layer.anchorPoint = CGPointMake(0.0, 0.0)
+  internal func drawLineIn(withRectRef rectRef: RectFramer) {
+    let refView = rectRef.viewRef
     
     let bezPath: UIBezierPath = UIBezierPath()
-    bezPath.moveToPoint(CGPointMake(0.0, 0.0))
-    bezPath.addLineToPoint(CGPointMake(CGRectGetMaxX(view.bounds), 0.0))
-    bezPath.addLineToPoint(CGPointMake(CGRectGetMaxX(view.bounds), CGRectGetMidY(view.bounds)))
-    bezPath.addLineToPoint(CGPointMake(0.0, CGRectGetMidY(view.bounds)))
+    bezPath.moveToPoint(rectRef.bottomLeft)
+    bezPath.addLineToPoint(rectRef.bottomRight)
+    bezPath.addLineToPoint(rectRef.topRight)
+    
+    //  bezPath.addLineToPoint(topLeft) // straight line
+    //  bezPath.addQuadCurveToPoint(topLeft, controlPoint: CGPointMake(midX, minY)) // gentle convex curve at top line of rect
+    bezPath.addQuadCurveToPoint(rectRef.topLeft, controlPoint: CGPointMake(rectRef.midX, rectRef.maxY)) // gentle concave curve of top line of rect
     bezPath.closePath()
     
     let drawingLayer = CAShapeLayer()
@@ -51,7 +84,7 @@ class ViewController: UIViewController {
     drawingLayer.fillColor = UIColor.clearColor().CGColor
     drawingLayer.lineWidth = 4.0
     
-    view.layer.addSublayer(drawingLayer)
+    refView.layer.addSublayer(drawingLayer)
   }
   
   
